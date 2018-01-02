b0141ab9c3dd178afb92771dfc00933a39c76da4
Alex <awu1337@gmail.com>
2018-01-02 14:44:17 -0600

    Fixed flush time to 6AM CST

--- botredis.py
+++ botredis.py
@@ -13,14 +13,14 @@ def check_user(redis_url, sender_id):
         count_time = int(r.lindex(sender_id, 0))
         count = int(r.lindex(sender_id, 1)) + 1
         if abs(cur_time - count_time) >= 60:
-            count = 0
+            count = 1
             r.lset(sender_id, 0, cur_time)
         r.lset(sender_id, 1, count)
         print('sender_id {0} has requested {1} time(s) in the past minute'.format(sender_id, count))
-        return 60 + count_time - cur_time if count >= 5 else 0
+        return 60 + count_time - cur_time if count > 5 else 0
     else:
         print('Adding sender_id {0}'.format(sender_id))
-        r.lpush(sender_id, 0, cur_time)
+        r.lpush(sender_id, 1, cur_time)
         return 0
 
 
@@ -40,8 +40,8 @@ def check_departures(redis_url, stop_id):
 def flush_redis(redis_url):
     """Flush redis from the first call of each day after 6 AM CST"""
     r = redis.from_url(redis_url)
-    cur_time = int(r.time()[0])
-    cur_day = cur_time - (cur_time % 86400) - 43200  # sets to 6 AM CST
+    cur_time = int(r.time()[0]) - 43200  # sets to 12PM UTC (6AM CST)
+    cur_day = cur_time - (cur_time % 86400)
     if r.exists('flush_time'):
         if cur_day != int(r.get('flush_time')):
             print('Flushing redis')
