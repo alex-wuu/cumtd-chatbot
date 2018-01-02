1fcd202c616e4b91368487b27ee985a9c4129b5f
Alex <awu1337@gmail.com>
2018-01-01 20:28:21 -0600

    Added daily redis flush

--- responder.py
+++ responder.py
@@ -1,73 +1,66 @@
-import datetime
 import iso8601
 import json
 import requests
 
 
 def message_response(token, recipient_id, message_text):
-	'''Sends message back to the original sender'''
-	params = {
-		"access_token": token
-	}
-	headers = {
-		"Content-Type": "application/json"
-	}
-	data = json.dumps({
-		"recipient": {
-			"id": recipient_id
-		},
-		"message": {
-			"text": message_text
-		}
-	})
-	r = requests.post('https://graph.facebook.com/v2.11/me/messages', params=params, headers=headers, data=data)
-	if r.status_code != 200:
-		print(r.status_code)
-		print(r.text)
+    """Sends message back to the original sender"""
+    params = {
+        "access_token": token
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post('https://graph.facebook.com/v2.11/me/messages', params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        print(r.status_code)
+        print(r.text)
 
 
 def departures_text(stop_name, departures):
-	'''Create message text containing bus departure times for a stop'''
-	print('Creating message with bus departures')
-	cur_time = iso8601.parse_date(departures['time'])
-	message_text = '{0} departures at {1}\n'.format(stop_name, cur_time.strftime('%I:%M %p'))
-	message_text += 'Data provided by CUMTD\n'  # needed for CUMTD TOS
-	if len(departures['departures']) == 0:
-		message_text += '\nNo buses are currently scheduled :('
-	for bus_time in departures['departures']:
-		message_text += '\n{0} in {1} min'.format(bus_time['headsign'], bus_time['expected_mins'])
-	return message_text.encode(encoding='utf-8')
+    """Create message text containing bus departure times for a stop"""
+    print('Creating message with bus departures')
+    cur_time = iso8601.parse_date(departures['time'])
+    message_text = '{0} departures at {1}\n'.format(stop_name, cur_time.strftime('%I:%M %p'))
+    message_text += 'Data provided by CUMTD\n'  # needed for CUMTD TOS
+    if len(departures['departures']) == 0:
+        message_text += '\nNo buses are currently scheduled :('
+    for bus_time in departures['departures']:
+        message_text += '\n{0} in {1} min'.format(bus_time['headsign'], bus_time['expected_mins'])
+    return message_text.encode(encoding='utf-8')
 
 
 def get_stop_id(key, base_url, received_text):
-	'''Search based on received text, then returns the stop_id and stop_name of closest match'''
-	print('Finding bus stops')
-	stop_id_url = base_url + '/{0}?key={1}&query={2}&count={3}'.format('getstopsbysearch', key, received_text, 3)
-	r = requests.get(stop_id_url)
-	if r.status_code == 200:
-		match_ids = r.json()
-		try:
-			stop_id = match_ids['stops'][0]['stop_id']
-			stop_name = match_ids['stops'][0]['stop_name']
-			return stop_id, stop_name
-		except IndexError:
-			return '', "Can't find a matching bus stop :("
-	else:
-		return '', "Can't find bus stops: Error {0}".format(r.status_code)
+    """Search based on received text, then returns the stop_id and stop_name of closest match"""
+    print('Finding bus stops')
+    stop_id_url = base_url + '/{0}?key={1}&query={2}&count={3}'.format('getstopsbysearch', key, received_text, 3)
+    r = requests.get(stop_id_url)
+    if r.status_code == 200:
+        match_ids = r.json()
+        try:
+            stop_id = match_ids['stops'][0]['stop_id']
+            stop_name = match_ids['stops'][0]['stop_name']
+            return stop_id, stop_name
+        except IndexError:
+            return '', "Can't find a matching bus stop :("
+    else:
+        return '', "Can't find bus stops: Error {0}".format(r.status_code)
 
 
 def get_departures(key, base_url, stop_id):
-	'''Get json of departures by stop_id'''
-	print('Finding departures for bus stop')
-	departures_url = base_url + '/{0}?key={1}&stop_id={2}'.format('getdeparturesbystop', key, stop_id)
-	r = requests.get(departures_url)
-	if r.status_code == 200:
-		return r.json()
-	else:
-		return "Can't get bus departures: Error {0}".format(r.status_code)
-
-
-def get_cur_time():
-	'''Returns current time of day in minutes'''
-	time_seconds = datetime.datetime.now().hour * 3600 + datetime.datetime.now().minute * 60 + datetime.datetime.now().second
-	return str(time_seconds).encode(encoding='utf-8')
+    """Get json of departures by stop_id"""
+    print('Finding departures for bus stop')
+    departures_url = base_url + '/{0}?key={1}&stop_id={2}'.format('getdeparturesbystop', key, stop_id)
+    r = requests.get(departures_url)
+    if r.status_code == 200:
+        return r.json()
+    else:
+        return "Can't get bus departures: Error {0}".format(r.status_code)
--- server.py
+++ server.py
@@ -14,66 +14,84 @@ BASE_URL = os.getenv('BASE_URL')
 REDIS_URL = os.getenv('REDIS_URL')
 
 
+def flush_redis():
+    """Flushes redis from the first call of each day after 6 AM CST"""
+    r = redis.from_url(REDIS_URL)
+    cur_time = int(r.time()[0])
+    cur_day = cur_time - (cur_time % 86400) - 43200  # sets to 6 AM CST
+    if r.exists('flush_time'):
+        if r.get('flush_time') != cur_day:
+            print('Flushing redis')
+            r.flushdb()
+            print('Setting last flush time to {0}'.format(cur_day))
+            r.set('flush_time', cur_day)
+    else:
+        print('Setting last flush time to {0}'.format(cur_day))
+        r.set('flush_time', cur_day)  # in case redis is externally flushed
+
+
 def check_redis(stop_id, stop_name):
-	'''Update redis for a stop_id if last update was more than a minute ago, then returns message_text to send'''
-	print('Checking redis for {0}: {1}'.format(stop_id, stop_name))
-	r = redis.from_url(REDIS_URL)
-	cur_time = responder.get_cur_time()
-	if r.exists(stop_id):
-		if int(cur_time) - int(r.lindex(stop_id, 0)) <= 60:
-			print('Already up-to-date')
-			message_text = r.lindex(stop_id, 1)
-		else:
-			print('Updating redis: cur_time {0}, last update {1}'.format(cur_time, r.lindex(stop_id, 0)))
-			departures = responder.get_departures(CUMTD_KEY, BASE_URL, stop_id)
-			message_text = departures if type(departures) == str else responder.departures_text(stop_name, departures)
-			r.lset(stop_id, 0, cur_time)
-			r.lset(stop_id, 1, message_text)
-	else:
-		print('Setting redis key')
-		departures = responder.get_departures(CUMTD_KEY, BASE_URL, stop_id)
-		message_text = departures if type(departures) == str else responder.departures_text(stop_name, departures)
-		r.lpush(stop_id, message_text)
-		r.lpush(stop_id, cur_time)
-	return message_text.decode(encoding='utf-8')
+    """Update redis for a stop_id if last update was more than a minute ago, then returns message_text to send"""
+    print('Checking redis for {0}: {1}'.format(stop_id, stop_name))
+    r = redis.from_url(REDIS_URL)
+    cur_time = int(r.time()[0])
+    if r.exists(stop_id):
+        if abs(cur_time - int(r.lindex(stop_id, 0))) <= 60:
+            print('Already up-to-date')
+            message_text = r.lindex(stop_id, 1)
+        else:
+            print('Updating redis: cur_time {0}, last update {1}'.format(cur_time, r.lindex(stop_id, 0)))
+            departures = responder.get_departures(CUMTD_KEY, BASE_URL, stop_id)
+            message_text = departures if type(departures) == str else responder.departures_text(stop_name, departures)
+            r.lset(stop_id, 0, cur_time)
+            r.lset(stop_id, 1, message_text)
+    else:
+        print('Setting redis key')
+        departures = responder.get_departures(CUMTD_KEY, BASE_URL, stop_id)
+        message_text = departures if type(departures) == str else responder.departures_text(stop_name, departures)
+        r.lpush(stop_id, message_text)
+        r.lpush(stop_id, cur_time)
+    return message_text.decode(encoding='utf-8')
 
 
 class handlerAPI(MethodView):
+    """Handles facebook GET and POST requests"""
 
-	def get(self):
-		'''Checks the verification token from the Facebook Graph API to identify the webhook'''
-		print('Handling Facebook verification')
-		if request.args.get('hub.mode') == 'subscribe' and request.args.get('hub.verify_token') == VERIFICATION_TOKEN:
-			print('Verification successful!')
-			return request.args.get('hub.challenge', default=''), 200
-		else:
-			return 'Verification failed; wrong verification token.', 403
+    def get(self):
+        """Checks the verification token from the Facebook Graph API to identify the webhook"""
+        print('Handling Facebook verification')
+        if request.args.get('hub.mode') == 'subscribe' and request.args.get('hub.verify_token') == VERIFICATION_TOKEN:
+            print('Verification successful!')
+            return request.args.get('hub.challenge', default=''), 200
+        else:
+            return 'Verification failed; wrong verification token.', 403
 
-	def post(self):
-		'''Handles messages from sender'''
-		print('Handling messages')
-		payload = request.get_json()
-		print(payload)
-		# Find the message text with sender and recipient IDs, and respond to each
-		if payload['object'] != 'page':
-			return 'ok', 200
-		for entry in payload['entry']:
-			for messaging_event in entry['messaging']:
-				try:
-					sender_id = messaging_event['sender']['id']
-					stop_id, stop_name = responder.get_stop_id(CUMTD_KEY, BASE_URL, messaging_event['message']['text'])
-					if stop_id != '':
-						message_text = check_redis(stop_id, stop_name)
-					else:
-						message_text = stop_name
-					print(message_text)
-					responder.message_response(PAGE_ACCESS_TOKEN, sender_id, message_text)
-				except IndexError:
-					pass
-		return 'ok', 200
+    def post(self):
+        """Handles messages from sender"""
+        print('Handling messages')
+        payload = request.get_json()
+        print(payload)
+        # Find the messages and sender IDs, and respond to each
+        if payload['object'] != 'page':
+            return 'ok', 200
+        flush_redis()
+        for entry in payload['entry']:
+            for messaging_event in entry['messaging']:
+                try:
+                    sender_id = messaging_event['sender']['id']
+                    stop_id, stop_name = responder.get_stop_id(CUMTD_KEY, BASE_URL, messaging_event['message']['text'])
+                    if stop_id != '':
+                        message_text = check_redis(stop_id, stop_name)
+                    else:
+                        message_text = stop_name
+                    print(message_text)
+                    responder.message_response(PAGE_ACCESS_TOKEN, sender_id, message_text)
+                except IndexError:
+                    pass
+        return 'ok', 200
 
 
 app.add_url_rule('/', view_func=handlerAPI.as_view('handler_api'))
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
