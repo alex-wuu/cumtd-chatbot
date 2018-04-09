40d7212269507b0af79215626b57338ce69d05ed
Alex <awu1337@gmail.com>
2018-04-09 03:42:17 -0500

    convert pluggable view to regular view functions

--- app/server.py
+++ app/server.py
@@ -19,7 +19,7 @@ CUSTOM_STOPS = os.getenv('CUSTOM_STOPS')
 def generate_response(messaging_event):
 
     """Return the user's ID and response text"""
-    
+
     sender_id = messaging_event['sender']['id']
 
     # Try block to check if user pressed get_started
@@ -89,48 +89,50 @@ def generate_response(messaging_event):
     return sender_id, message_text
 
 
-class handlerAPI(MethodView):
+@app.route('/', methods=['GET'])
+def fb_get():
 
-    """Handles Facebook GET and POST requests"""
+    """Check the verification token from the Facebook Graph API to identify the webhook"""
 
-    def get(self):
+    print('Handling Facebook verification')
+    if request.args.get('hub.mode') == 'subscribe' and \
+       request.args.get('hub.verify_token') == VERIFICATION_TOKEN:
+        print('Verification successful!')
+        return request.args.get('hub.challenge', default=''), 200
+    else:
+        return 'Verification failed; wrong verification token.', 403
 
-        """Check the verification token from the Facebook Graph API to identify the webhook"""
 
-        print('Handling Facebook verification')
-        if request.args.get('hub.mode') == 'subscribe' and request.args.get('hub.verify_token') == VERIFICATION_TOKEN:
-            print('Verification successful!')
-            return request.args.get('hub.challenge', default=''), 200
-        else:
-            return 'Verification failed; wrong verification token.', 403
+@app.route('/', methods=['POST'])
+def fb_post():
 
-    def post(self):
+    """Handle messages from sender"""
 
-        """Handle messages from sender"""
+    print('Handling messages')
+    payload = request.get_json()
+    print(payload)
 
-        print('Handling messages')
-        payload = request.get_json()
-        print(payload)
-
-        # Find the messages then respond to each
-        if payload['object'] != 'page':
-            return 'ok', 200
-        botredis.flush_redis(REDIS_URL)
-        for entry in payload['entry']:
-            for messaging_event in entry['messaging']:
-                try:
-                    sender_id, message_text = generate_response(messaging_event)
-                    if message_text != 'location_request':
-                        print(message_text)
-                        responder.send_text(PAGE_ACCESS_TOKEN, FB_URL, sender_id, message_text)
-                    else:
-                        responder.send_location_button(PAGE_ACCESS_TOKEN, FB_URL, sender_id)
-                except KeyError:
-                    pass
+    # Find the messages then respond to each
+    if payload['object'] != 'page':
         return 'ok', 200
+    botredis.flush_redis(REDIS_URL)
+    for entry in payload['entry']:
+        for messaging_event in entry['messaging']:
+            try:
+                sender_id, message_text = generate_response(messaging_event)
+                if message_text != 'location_request':
+                    print(message_text)
+                    responder.send_text(PAGE_ACCESS_TOKEN, FB_URL, sender_id, message_text)
+                else:
+                    responder.send_location_button(PAGE_ACCESS_TOKEN, FB_URL, sender_id)
+            except KeyError:
+                pass
+    return 'ok', 200
 
 
-app.add_url_rule('/', view_func=handlerAPI.as_view('handler_api'))
+@app.route('/ping_me')
+def ping_me():
+    return 'I am awake!'
 
 if __name__ == '__main__':
     app.run()
